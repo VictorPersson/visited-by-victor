c7db28ce848581231ea102d1a1ec2c0426132acd
VictorPersson <vp-96@hotmail.com>
2019-05-22 15:57:31 +0200

    Implemented vue starter files form 'vue create'

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,23 +6,17 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header/Header.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+      <h1>Heading</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

57b8eb447c4c6cfe93865cc519eca3acfdb35947
VictorPersson <vp-96@hotmail.com>
2019-06-16 14:24:57 +0200

    Moved database connection data to config file

--- client/src/main.js
+++ client/src/main.js
@@ -10,12 +10,12 @@ new Vue({
   created() {
     firebase.initializeApp({
         apiKey: APIKey.firebaseApi,
-        authDomain: 'victors-visited.firebaseapp.com',
-        databaseURL: 'https://victors-visited.firebaseio.com',
-        projectId: 'victors-visited',
-        storageBucket: 'victors-visited.appspot.com',
-        messagingSenderId: '873869873251',
-        appId: '1:873869873251:web:6374d30d639062d7'
+        authDomain: APIKey.authDomain,
+        databaseURL: APIKey.databaseURL,
+        projectId: APIKey.projectId,
+        storageBucket: APIKey.storageBucket,
+        messagingSenderId: APIKey.messagingSenderId,
+        appId: APIKey.appId
     })
   },
 }).$mount('#app')
